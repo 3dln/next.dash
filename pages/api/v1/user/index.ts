5183ff6f91b6d49e592390c37fcd6705e29d3e62
Ashkan Ashtiani <ashkan.ashtiani@gmail.com>
2021-03-15 13:36:40 +0330

    cached mongo client for further usage

--- pages/api/v1/user/index.ts
+++ pages/api/v1/user/index.ts
@@ -1,11 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 import { MongoHelper } from 'utils/db/connect';
+import logger from 'utils/logger';
 
 const handler = nc();
 
 handler.get((req: NextApiRequest, res: NextApiResponse) => {
-  MongoHelper.connect();
+  if (MongoHelper.cachedClient) {
+    logger.debug('DB is already connected');
+  } else MongoHelper.connect();
   res.json({ success: true });
 });
 
--- utils/db/connect.ts
+++ utils/db/connect.ts
@@ -1,28 +1,38 @@
-import { MONGODB_URI } from 'configs';
+// import { MONGODB_URI } from 'configs';
 import { MongoClient } from 'mongodb';
 import logger from 'utils/logger';
 export class MongoHelper {
-  public static client: MongoClient;
+  public static cachedClient: MongoClient;
   public static connect(): Promise<MongoClient> {
     return new Promise((resolve, reject) => {
-      MongoClient.connect(
-        'mongodb://localhost:27017/testdb',
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        },
-        (err, client: MongoClient) => {
-          if (err) {
-            logger.error('connection to mongo db failed');
-            logger.error(err);
-            reject(err);
-          } else {
-            logger.log('info', 'Successfully connected to mongodb');
-            MongoHelper.client = client;
-            resolve(client);
-          }
+      const prod = process.env.NODE_ENV === 'production';
+      const MONGODB_URI = prod ? process.env['MONGODB_URI'] : process.env['MONGODB_URI_LOCAL'];
+      if (!MONGODB_URI) {
+        if (prod) {
+          logger.error('No mongo connection string. Set MONGODB_URI environment variable');
+        } else {
+          logger.error('No mongo connection string, set MONGODB_URI_LOCAL environment variable');
         }
-      );
+        reject('Mongodb Connection string not found');
+      } else
+        MongoClient.connect(
+          MONGODB_URI,
+          {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          },
+          (err, client: MongoClient) => {
+            if (err) {
+              logger.error('connection to mongo db failed');
+              logger.error(err);
+              reject(err);
+            } else {
+              logger.log('info', 'Successfully connected to mongodb');
+              MongoHelper.cachedClient = client;
+              resolve(client);
+            }
+          }
+        );
     });
   }
 }
